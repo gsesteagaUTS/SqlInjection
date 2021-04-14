39071afc4013ad081df6c20e841b19367585f928
Gilberto Sesteaga <gisego03@gmail.com>
2021-04-14 12:35:46 -0700

    Initial commit

---
+++ SqlInjection/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SqlInjection.Models;
+
+namespace SqlInjection.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        [HttpGet]
+        public User Login(User user){
+
+            return new User();
+        }
+
+    }
+}
